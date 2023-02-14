aca633628cd39629eef884738ed05089dd1a66ca
Jaydeep Shelake <jaydeepshelake10@gmail.com>
2023-02-14 19:18:19 +0530

    m: added color pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,39 @@
-import {createWebHistory,createRouter} from "vue-router"
-import HomePage from '../views/HomePage.vue'
-import SearchVue from '../views/Search.vue'
-import PostPage from '../views/Posts.vue'
-import CallbackPage from '../views/CallbackPage.vue'
-const routes =[
-    {
-        name:"HomePage",
-        path:"/",
-        component:HomePage,
-    },
-    {
-        name:"SearchPage",
-        path:"/search",
-        component:SearchVue
-    },
-    {
-        name:"PostPage",
-        path:"/posts",
-        component:PostPage
-    },
-    {
-        name:"CallbackPage",
-        path:"/callback",
-        component:CallbackPage
-    }
-]
+import { createWebHistory, createRouter } from "vue-router";
+import HomePage from "../views/HomePage.vue";
+import SearchVue from "../views/Search.vue";
+import PostPage from "../views/Posts.vue";
+import CallbackPage from "../views/CallbackPage.vue";
+import AvtarColor from "@/views/AvtarColors.vue";
+const routes = [
+  {
+    name: "HomePage",
+    path: "/",
+    component: HomePage,
+  },
+  {
+    name: "SearchPage",
+    path: "/search",
+    component: SearchVue,
+  },
+  {
+    name: "PostPage",
+    path: "/posts",
+    component: PostPage,
+  },
+  {
+    name: "CallbackPage",
+    path: "/callback",
+    component: CallbackPage,
+  },
+  {
+    name: "AvatarPage",
+    path: "/avatar-color",
+    component: AvtarColor,
+  },
+];
 const router = createRouter({
-    history:createWebHistory(),
-    routes,
-})
+  history: createWebHistory(),
+  routes,
+});
 
-export default router
+export default router;
---
+++ src/views/AvtarColors.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="w-full">
+    <AvtarColor v-for="color in colorsArray" :key="color" :mainColor="color" />
+  </div>
+</template>
+
+<script setup>
+import AvtarColor from "@/components/AvtarColor.vue";
+const colorsArray = [
+  "#FF6B6B",
+  "#FCE831",
+  "#25C364",
+  "#5B99F0",
+  "#8F4EEA",
+  "#F9774F",
+  "#FBB02D",
+  "#0EC893",
+  "#758BFD",
+  "#DB48DA",
+  "#F4A549",
+  "#C8CC29",
+  "#05A8AA",
+  "#77B6EA",
+  "#EF5898",
+  "#FF9D66",
+  "#CFEC1E",
+  "#A1EB78",
+  "#77C7C2",
+  "#C777BF",
+];
+</script>
+<style scoped>
+.w-full {
+  background: black;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  padding: 40px;
+}
+</style>
